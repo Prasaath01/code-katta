99073dc015d97bd3079db153f9c2e6af2681fbe3
Prasaath01 <50820410+Prasaath01@users.noreply.github.com>
2019-05-22 14:17:02 +0530

    Update sumofint.C

--- sumofint.C
+++ sumofint.C
@@ -2,15 +2,19 @@
 
 int main()
 {
-   int i,n,k,sum=0,a[25];
+   int i,n,k,a[25];
+   int sum=0;
    scanf("%d%d",&n,&k);
+   
    for(i=1;i<=n;i++)
    {
        scanf("%d",&a[i]);
    }
+   
    for(i=1;i<=k;i++)
    {
        sum=sum+a[i];
    }
    printf("%d",sum);
+   return 0;
 }
