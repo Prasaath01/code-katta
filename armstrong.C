a6e15ce0da7a9c757daa671ac8e2a8b0b4f0a26f
Prasaath01 <50820410+Prasaath01@users.noreply.github.com>
2019-05-23 22:59:03 +0530

    Update armstrong.C

--- armstrong.C
+++ armstrong.C
@@ -1,8 +1,11 @@
 #include <stdio.h>
+
 #include <math.h>
 
 int main()
+
 {
+    
     int num, orgNum, rem, result = 0, n = 0 ;
 
     scanf("%d", &num);
@@ -18,11 +21,16 @@ int main()
     orgNum = num;
 
     while (orgNum != 0)
+    
     {
+       
         rem = orgNum%10;
         result += pow(rem, n);
         orgNum /= 10;
+    
+        
     }
+    
 
     if(result == num)
         printf("yes");
