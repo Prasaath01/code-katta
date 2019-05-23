649bc37b059badb06e186a448ad068ada3771190
Prasaath01 <50820410+Prasaath01@users.noreply.github.com>
2019-05-23 23:19:07 +0530

    Create multipleofnum.C

---
+++ multipleofnum.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+        long int sum=0,i,n;
+        scanf("%ld",&n);
+        for(i=1;i<=n;i++)
+        {
+            printf("%d ",5*i);
+        }
+        
+        getch();
+}
