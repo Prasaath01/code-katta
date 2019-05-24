e7a232f105d328fc54ecf7c00de41a2c35f36cfa
Prasaath01 <50820410+Prasaath01@users.noreply.github.com>
2019-05-24 11:33:23 +0530

    Create sumarthm.C

---
+++ sumarthm.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    
+    int sum,a,m,e,i;
+    
+    sum=0;
+    
+    scanf("%d%d%d",&m,&a,&e);
+    sum=(m*(2*a+(m-1)*e))/2;
+   
+    printf("%d",sum);
+            
+    return 0;
+}
