304c6755de400a0f47e66bd5ea56893ca53bf7c1
Prasaath01 <50820410+Prasaath01@users.noreply.github.com>
2019-05-30 10:23:30 +0530

    Create numofspl.C

---
+++ numofspl.C
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+ 
+int main()
+{
+    char s[200];
+    int count = 0, i,a=0,b=0;
+   
+    
+    scanf("%[^\n]s", s);
+   
+    for (i = 0;s[i] != '\0';i++)
+    {
+         a=isdigit(s[i]);
+         b=isalpha(s[i]);
+        if(a==0 && b==0)
+        {
+            count++; 
+        }    
+    }
+    printf("%d", count );
+    return 0;
+}
